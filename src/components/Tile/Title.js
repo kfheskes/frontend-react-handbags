73195d44217ae76dfb886a7f6206c49348bcff8a
kfheskes <kayarneheskes@hotmail.com>
2023-05-12 09:35:38 +0200

    uitwerkingen12Mei0935

---
+++ src/components/Tile/Title.js
@@ -0,0 +1,14 @@
+function Tile({ label, image, imageDescription, paragraph, price}){
+
+
+    return(
+        <article className="article">
+            <span>{ label }</span>
+            <img src={ image } alt={imageDescription}/>
+            <p>{ paragraph }</p>
+            <h4>{ price }</h4>
+        </article>
+    )
+}
+
+export default Product;
